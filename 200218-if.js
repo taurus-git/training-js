8c2cc292d6e5ff6d3b091f6521be5acd3fbf7dd3
taurus-git <t.1113@ya.ru>
2018-02-20 10:13:39 +0200

    Solved tasks with if, else

---
+++ 200218-if.js
@@ -0,0 +1,37 @@
+'use strict'
+
+/* the task
+if -> ?
+if (a + b < 4){
+	result = 'Мало';
+} else {
+	result = 'Много';
+}*/
+
+//decision
+/*result = (a + b < 4) ? 'Мало' : 'Много';*/
+
+
+
+//the task
+//if + else -> ?
+
+/*var message;
+
+if (login == 'Вася') {
+	message = 'Привет';
+} else if (login == 'Директор') {
+	message = 'Здравствуйте';
+} else if (login == '') {
+	message = 'Нет логина';
+} else {
+	message = '';
+}*/
+
+
+/*decision
+var message = (login == 'Вася') ? 'Привет' : 
+	(login == 'Директор') ? 'Здравствуйте' :
+	(login == '') ? 'Нет логина' : 
+	'';
+*/
