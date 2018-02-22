3a8856fbf0caebfed16c963c2e171565122b6c56
taurus-git <t.1113@ya.ru>
2018-02-22 11:13:30 +0200

    task with for

--- studyScript.js
+++ studyScript.js
@@ -1,6 +1,15 @@
 'use strict'
 //for
+/*
 for (var i = 0; i < 10; i++) {
     console.log('Привет');
     console.log('Кеша');
+}
+*/
+//for from 2 to 10
+for (var evenNumber = 2; evenNumber < 10; evenNumber++ ) {
+
+    if (evenNumber % 2 == 0) continue;
+
+    alert(evenNumber);
 }
