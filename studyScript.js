68126d94b431210b1e8f98477950e565cf5d81da
taurus-git <t.1113@ya.ru>
2018-02-23 10:44:11 +0200

    second task with for

--- studyScript.js
+++ studyScript.js
@@ -7,9 +7,31 @@ for (var i = 0; i < 10; i++) {
 }
 */
 //for from 2 to 10
+/*
 for (var evenNumber = 2; evenNumber < 10; evenNumber++ ) {
 
     if (evenNumber % 2 == 0) continue;
 
     alert(evenNumber);
-}
+}*/
+//task
+//prompt num may be > 100
+/*
+var num;
+
+do {
+    num = prompt ('Введите число больше 100', '');
+} while (num <= 100 && num != null);*/
+
+//task
+//decision
+/*
+nextPrime:
+    for (var i = 2; i <= 10; i++) {
+
+        for (var j = 2; j < i; j++) {
+            if (i % j == 0) continue nextPrime;
+        }
+
+        alert (i);
+    }*/
