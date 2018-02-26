d42170fec1c9885008533b5a9b19d51d484ad925
taurus-git <t.1113@ya.ru>
2018-02-26 17:45:07 +0200

    end of the tasks with "switch"

--- studyScript.js
+++ studyScript.js
@@ -70,7 +70,7 @@ if (browser == 'IE') {
 
 //task
 //if -> switch
-var a = +prompt('a?', '');
+/*var a = +prompt('a?', '');
 
 if (a == 0) {
     alert( 0 );
@@ -81,8 +81,28 @@ if (a == 1) {
 
 if (a == 2 || a == 3) {
     alert( '2,3' );
-}
+}*/
 
+//decision
+/*
+var a = +prompt('a?', '');
+
+switch(a) {
+    case 0:
+        alert(0);
+        break;
+    case 1:
+        alert(1);
+        break;
+    case 2:
+    case 3:
+        alert('2, 3');
+        break;
+    default:
+        alert('Я таких значений не знаю');
+        break;
+}
+*/
 
 
 
