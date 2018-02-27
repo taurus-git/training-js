18804fe98d7c66a7be92bfd5ea391ab0603914d1
taurus-git <t.1113@ya.ru>
2018-02-27 22:45:48 +0200

    tasks with "function"

--- studyScript.js
+++ studyScript.js
@@ -153,4 +153,25 @@ function calcMinNumber(a, b){
 function calcMinNumber(a, b){
    return a < b ? a : b;
 }*/
+//task
+function pow(x, n) {
+    var result = x;
+
+    for (var i = 1; i < n; i++) {
+        result *= x;
+    }
+
+    return result;
+}
+
+var x = prompt("x?", '');
+var n = prompt("n?", '');
+
+if (n <= 1) {
+    alert('Степень ' + n +
+        'не поддерживается, введите целую степень, большую 1'
+    );
+} else {
+    alert( pow(x, n) );
+}
 
