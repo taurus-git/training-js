9e88b8e3d1f7deb599a9140df3e96b64ca5265db
taurus-git <t.1113@ya.ru>
2018-03-23 11:40:27 +0200

    task with objects. part 2

--- studyScript.js
+++ studyScript.js
@@ -567,7 +567,47 @@ for (var name in salaries) {
 }
 
 alert( sum );*/
+//task
+/*var salaries = {
+  'Вася': 100,
+  'Петя': 300,
+  'Даша': 250,
+  'Иван': 400
 
+};
+
+var max = 0;
+var maxName = '';
+for (var name in salaries) {
+    if (max < salaries[name]) {
+        max = salaries[name];
+        maxName = name;
+    }
+}
+
+alert( maxName || 'нет сотрудников' );*/
+//task
+/*var menu = {
+    width: 200,
+    height: 300,
+    title: 'My menu'
+};
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n)
+}
+
+function multiplyNumeric(obj) {
+    for (var key in obj) {
+        if (isNumeric(obj[key])){
+            obj[key] *= 2;
+        }
+    }
+};
+
+multiplyNumeric(menu);
+
+alert( 'menu.width=' + menu.width + ' height=' + menu.height + ' title=' + menu.title);*/
 
 
 
