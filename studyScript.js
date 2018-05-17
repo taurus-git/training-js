a5ce94cb78f28cb83ff7c3bc1ccdbfd2eea0aec0
taurus-git <t.1113@ya.ru>
2018-05-17 11:09:10 +0300

    decorators

--- studyScript.js
+++ studyScript.js
@@ -1130,7 +1130,88 @@ alert( applyAll(sum, 1, 2, 3) );
 alert( applyAll(mul, 2, 3, 4) );
 alert( applyAll(Math.max, 2, -2, 3) );
 alert( applyAll(Math.min, 2, -2, 3) );*/
+//task
+//decorator
+//decision by js.ru
+/*function work(a) {
 
+}
+
+function makeLogging(f, log) {
+
+    function wrapper(a) {
+        log.push(a);
+        return f.call(this, a);
+    }
+
+    return wrapper;
+}
+
+var log = [];
+work = makeLogging(work, log);
+
+work(1);
+work(5);
+
+for (var i = 0; i < log.length; i++) {
+    alert( 'Лог:' + log[i] );
+}*/
+//task
+//decorator - 2
+//decision by js.ru
+/*function work(a, b) {
+    alert( a + b );
+}
+
+function makelogging(f, log) {
+
+    function wrapper() {
+        log.push([].slice.call(arguments));
+        return f.apply(this, arguments);
+    }
+
+    return wrapper;
+}
+
+var log = [];
+work = makelogging(work, log);
+
+work(1, 2);
+work(4, 5);
+
+for (var i = 0; i < log.length; i++) {
+    var args = log[i];
+    alert( 'Лог:' + args.join() );
+}*/
+//task
+//decorator - 3
+//decision by js.ru
+
+/*
+function f(x) {
+    return Math.random()*x;
+}
+
+function makeCaching(f) {
+    var cache = {};
+
+    return function(x) {
+        if (!(x in cache)) {
+            cache[x] = f.call(this, x);
+        }
+        return cache[x];
+    };
+}
+
+f = makeCaching(f);
+
+var a = f(1);
+var b = f(1);
+alert( a == b );
+
+b = f(2);
+alert( a == b );
+*/
 
 
 
