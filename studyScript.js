9ddcde5d46f2cae007f9d56475ce0ffdc2998c76
taurus-git <t.1113@ya.ru>
2018-05-18 11:37:58 +0300

    typeof

--- studyScript.js
+++ studyScript.js
@@ -1212,7 +1212,26 @@ alert( a == b );
 b = f(2);
 alert( a == b );
 */
+//task
+//typeof
+//decision by js.ru
+/*function formatDate(date) {
+    if (typeof date == 'number') {
+        date = new Date(date * 1000);
+    } else if (typeof date == 'string') {
+        date = new Date(date);
+    } else if (Array.isArray(date)) {
+        date = new Date(date[0], date[1], date[2]);
+    }
 
+    return date.toLocaleString('ru', {day: '2-digit', month: '2-digit', year: '2-digit'});
+}
+
+
+alert( formatDate('2011-10-02') );
+alert( formatDate(1234567890) );
+alert( formatDate([2014, 0, 1]) );
+alert( formatDate(new Date(2014, 0, 1)) );*/
 
 
 
