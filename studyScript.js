ccb1920e773e31a3dfa98a511ed738ba54365460
taurus-git <taurusgit1531>
2018-07-09 11:21:56 +0300

    task with closures

--- studyScript.js
+++ studyScript.js
@@ -2649,9 +2649,42 @@ users.forEach(function (user) {
     console.log( user.name );
 });*/
 //task
+//decision by js.ru
+/*function filter(arr, func) {
+    var result = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        var val = arr[i];
+        if (func(val)) {
+            result.push(val);
+        }
+    }
+
+    return result;
+}
+
+function inBetween(a, b) {
+    return function(x) {
+        return x >= a && x <= b;
+    };
+}
+
+function inArray(arr) {
+    return function(x) {
+        return arr.indexOf(x) != -1;
+    };
+}
 
 
+var arr = [1, 2, 3, 4, 6, 7];
 
+alert(filter(arr, function(a) {
+    return a % 2 == 0;
+})); // 2, 4, 6
+
+alert( filter(arr, inBetween(3, 6)) ); // 3,4,5,6
+
+alert( filter(arr, inArray([1, 2, 10])) ); // 1,2*/
 
 
 
