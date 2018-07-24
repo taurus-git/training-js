996c1c435ffbb6698778d813b22d104680a31c2b
taurus-git <taurusgit1531>
2018-07-24 12:21:21 +0300

    task with call and apply

--- studyScript.js
+++ studyScript.js
@@ -2905,10 +2905,29 @@ Article.showStats();*/
 }
 console.log( sumArgs(1, 2, 3) );*/
 //task
-function applyAll() {
-
+//decision by js.ru
+/*function sum() {
+    return [].reduce.call(arguments, function(a, b) {
+        return a + b;
+    });
 }
 
+function mul() {
+    return [].reduce.call(arguments, function(a, b) {
+        return a * b;
+    });
+}
+
+function applyAll(func) {
+    return func.apply(this, [].slice.call(arguments, 1));
+}
+
+
+alert( applyAll(sum, 1, 2, 3) ); // 6
+alert( applyAll(mul, 2, 3, 4) ); // 24
+alert( applyAll(Math.max, 2, -2, 3) ); // 3
+alert( applyAll(Math.min, 2, -2, 3) ); // -2*/
+//task
 
 
 
