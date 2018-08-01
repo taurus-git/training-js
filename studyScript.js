d7795d50ecb57a3f03fd136969f9635046bd210b
taurus-git <t.1113@ya.ru>
2018-08-01 11:27:38 +0300

    task with __proto__

--- studyScript.js
+++ studyScript.js
@@ -3451,11 +3451,32 @@ var coffeeMachine = new CoffeeMachine(10000);
 coffeeMachine.enable();
 coffeeMachine.run();
 coffeeMachine.disable(); // остановит работу, ничего не выведет*/
+//task
+//prototype
+/*//decision by js.ru
+var head = {
+   glasses: 1
+};
 
+var table = {
+    pen: 3
+};
+table.__proto__ = head;
 
+var bed = {
+    sheet: 1,
+    pillow: 2
+};
+bed.__proto__ = table;
 
+var pockets = {
+    money: 2000
+};
+pockets.__proto__ = bed;
 
-
+console.log( pockets.pen );
+console.log( bed.glasses );
+console.log( table.money );*/
 
 
 
