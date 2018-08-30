f4ab136d2a537165386626cbbefda8ed8e62d163
taurus-git <taurusgit1531>
2018-08-30 12:31:00 +0300

    tasks with multi insert

--- studyScript.js
+++ studyScript.js
@@ -4047,7 +4047,10 @@ function clockStop() {
     timerId = null;
 }
 *!/*/
+//task
+/*var ul = document.body.children[0];
 
+ul.insertAdjacentHTML("beforeEnd", "<li>3</li><li>4</li><li>5</li>");*/
 
 
 
